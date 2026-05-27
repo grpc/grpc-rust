edc5166a0a6fde4b699faf8a07ad2321a9b1816f
Doug Fawley <dfawley@google.com>
2026-05-27 10:00:07 -0700

    interop stop building plugin

--- interop/test.sh
+++ interop/test.sh
@@ -38,11 +38,7 @@ esac
 
 ARG="${1:-""}"
 
-# Skip building in github actions as the workflow already builds it (to
-# make sure the cache works).
-if [ -z "${GITHUB_ACTIONS:-}" ]; then
-  (cd interop && cargo build --bins)
-fi
+(cd interop && cargo build --bins)
 
 SERVER="interop/bin/server_${OS}_amd64${EXT}"
 

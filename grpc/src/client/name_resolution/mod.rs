e98a9c94177ec1ba2fc28b7da872f4dbd52e774e
Nathaniel Ford <nathaniel.ford@gmail.com>
2026-07-07 15:37:41 +0000

    Comment removal

--- grpc/src/client/name_resolution/mod.rs
+++ grpc/src/client/name_resolution/mod.rs
@@ -211,11 +211,6 @@ pub(crate) struct ResolverOptions {
     /// This value is either the result of the `default_authority` method of
     /// this `ResolverBuilder`, or another string if the channel was explicitly
     /// configured to override the default.
-    // TODO(nathanielford): This field is currently unused. Review type if/when
-    //     it is used.
-    // - Channels reference SecurityOpts, which hold the value in an Authority
-    //   object.
-    // - All other references currently use "ignored" as the authority.
     pub authority: String,
 
     /// The runtime which provides utilities to do async work.

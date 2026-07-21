825280447ce313187267ae22ee67df0e9f7a9d07
Arjan Bal <arjansbal@google.com>
2026-07-21 13:36:51 +0530

    fix protoc detection for CI

--- grpc-benchmark/build.rs
+++ grpc-benchmark/build.rs
@@ -51,8 +51,11 @@ fn main() {
     let services_tonic = out_dir.join("tonic");
 
     // TODO: Use gRPC servers when available.
-    let protoc_path = protoc_gen_rust_grpc::protoc();
-    unsafe { env::set_var("PROTOC", &protoc_path) };
+    if protoc_gen_rust_grpc::protoc().exists() {
+        unsafe {
+            env::set_var("PROTOC", protoc_gen_rust_grpc::protoc());
+        }
+    }
 
     let _ = std::fs::create_dir(services_tonic.clone());
     tonic_prost_build::configure()

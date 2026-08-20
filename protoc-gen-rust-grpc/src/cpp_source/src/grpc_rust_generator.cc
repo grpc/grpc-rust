44a96f9875931a27c602aec55d7181607abeda01
Nathaniel Ford <nathaniel.ford@gmail.com>
2026-08-20 22:12:02 +0000

    Add dead-code ignore annotation to avoid compilation errors when using the same protobuf for both a gRPC-Rust client and a Tonic server.

--- protoc-gen-rust-grpc/src/cpp_source/src/grpc_rust_generator.cc
+++ protoc-gen-rust-grpc/src/cpp_source/src/grpc_rust_generator.cc
@@ -391,6 +391,13 @@ static void GenerateClient(const Service &service, Printer &printer,
       R"rs(
       /// Generated client implementations.
       pub mod $client_mod$ {
+          #![allow(
+              unused_variables,
+              dead_code,
+              missing_docs,
+              clippy::wildcard_imports,
+              unused_imports,
+          )]
           use grpc::client::*;
           use grpc_protobuf::*;
 
